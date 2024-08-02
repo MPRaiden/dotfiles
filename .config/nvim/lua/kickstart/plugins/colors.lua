13b1863c9c76b88ce8eb526a228c55d28fa96697
mpr <marinpericradan1@gmail.com>
2024-08-02 19:42:18 +0200

    vim & kitty theme updates

--- .config/nvim/lua/kickstart/plugins/colors.lua
+++ .config/nvim/lua/kickstart/plugins/colors.lua
@@ -1,25 +1,45 @@
+function ColorMyPencils(color)
+  color = color or 'rose-pine'
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end
+
 return {
-  --'folke/tokyonight.nvim',
-  'rose-pine/neovim',
-  priority = 1000,
-  config = function()
-    local transparent = true -- set to true if you would like to enable transparency
+  {
+    'folke/tokyonight.nvim',
+    config = function()
+      require('tokyonight').setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = 'moon', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = 'dark', -- style for sidebars, see below
+          floats = 'dark', -- style for floating windows
+        },
+      }
+    end,
+  },
 
-    require('rose-pine').setup {
-      -- Uncomment the following if you are using tokyonight
-      -- variant = 'moon', -- Variant options are main, moon, dawn
-      -- style = 'night', -- the theme comes in three styles: `storm`, `moon`, a darker variant `night`, and `day`
-      transparent = transparent,
-      styles = {
-        sidebars = transparent and 'transparent' or 'dark',
-        floats = transparent and 'transparent' or 'dark',
-        italic = false,
-      },
-    }
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      require('rose-pine').setup {
+        disable_background = true,
+      }
 
-    -- Apply the colorscheme. Uncomment the one you are using.
-    -- vim.cmd 'colorscheme tokyonight'
-    vim.cmd 'colorscheme rose-pine' -- change between rose-pine alternatives here
-    --vim.cmd 'highlight Normal guibg=NONE ctermbg=NONE' -- uncomment for transparency
-  end,
+      vim.cmd 'colorscheme rose-pine'
+
+      ColorMyPencils 'tokyonight'
+    end,
+  },
 }
